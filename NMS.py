00efc359eee154ab3829f2b5334f79eefadc27ff
Kassim <jondeaukassim@gmail.com>
2022-11-25 16:39:07 +0100

    end

--- NMS.py
+++ NMS.py
@@ -47,6 +47,59 @@ def start():
         OutOctets = 0
         InOctets = 0
 
+        
+
+        def construct_object_types(self,liste_oids):
+        object_types = []
+        for oid in list_of_oids:
+            object_types.append(hlapi.ObjectType(hlapi.ObjectIdentity(oid)))
+        return object_types
+
+    def construct_value_pairs(self, list_of_pairs):
+        pairs = []
+        for key, value in list_of_pairs.items():
+            pairs.append(hlapi.ObjectType(hlapi.ObjectIdentity(key), value))
+        return pairs
+
+    def cast(self, value):
+        try:
+            return int(value)
+        except (ValueError, TypeError):
+            try:
+                return float(value)
+            except (ValueError, TypeError):
+                try:
+                    return str(value)
+                except (ValueError, TypeError):
+                    pass
+        return value
+
+    def fetch(self, handler, count):
+        result = []
+        for i in range(count):
+            try:
+                error_indication, error_status, error_index, var_binds = next(handler)
+                if not error_indication and not error_status:
+                    items = {}
+                    for var_bind in var_binds:
+                        items[str(var_bind[0])] = self.cast(var_bind[1])
+                    result.append(items)
+                else:
+                    raise RuntimeError('Got SNMP error: {0}'.format(error_indication))
+            except StopIteration:
+                break
+        return result
+
+    def get(self, target, oids, port=161, engine=hlapi.SnmpEngine(), context=hlapi.ContextData()):
+        handler = hlapi.nextCmd(
+            engine,
+            hlapi.CommunityData('snmp_kas'),
+            hlapi.UdpTransportTarget((target, port)),
+            context,
+            *self.construct_object_types(oids)
+        )
+        return self.fetch(handler, 1)[0]
+
         data = (
         ObjectType(ObjectIdentity('IF-MIB', 'ifDescr')),
         ObjectType(ObjectIdentity('IF-MIB', 'ifInOctets')),
@@ -129,7 +182,7 @@ def start():
                 print("This is the uptime " + str(round(varBinds[0][1]/100/60)) + " minutes.")
     #
 
-
+"""
 
 
 
